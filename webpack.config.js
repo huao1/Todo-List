dfeb668121c68ca5cb602e74cfdc1d9f623f0826
huao1 <huao.cheng@gmail.com>
2024-03-09 21:16:39 -0500

    Completed setup for Webpack and all files needed are also included.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+			title: 'Webpack Tutorial',
+			filename: 'index.html',
+			inject: 'head',
+			scriptLoading: 'defer',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
