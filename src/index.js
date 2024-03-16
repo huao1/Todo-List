8d5fd6a7a1f5a2d80699b97e2c3db61ebbd98f3a
huao1 <huao.cheng@gmail.com>
2024-03-16 17:12:16 -0400

    Hard coded a few to do items as an example and completed all of the styling for it. Also added an animation for when the list item is being hovered over with the mouse.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import "./index.css"
 import check from "./image/check.png"
+import trash from "./image/trash.png"
+import edit from "./image/edit.png"
+import emptyBox from "./image/blank-check-box.png"
 
 const logo = document.querySelector("#logo")
 logo.setAttribute('src', check)
+
+const trashIcon = document.querySelector(".trash")
+trashIcon.setAttribute('src', trash)
+
+const editIcon = document.querySelector(".edit")
+editIcon.setAttribute('src', edit)
+
+const empty = document.querySelector(".empty")
+empty.setAttribute('src', emptyBox) 
 
 console.log("This works")
